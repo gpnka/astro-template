4e226546049c5b8d4827916f085a44d743c311d7
Gapeienko Artem <185092183+gpnka@users.noreply.github.com>
2026-08-10 00:44:27 +0200

    Remove baseUrl from TypeScript configuration
    deprecated in ts 7.0

--- patches/02_tsconfig.sh
+++ patches/02_tsconfig.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 jq '
   .compilerOptions.plugins = [{ "name": "@astrojs/ts-plugin" }] |
   .compilerOptions.verbatimModuleSyntax = true |
-  .compilerOptions.baseUrl = "." |
   .compilerOptions.paths = { "@/*": ["./src/*"] }
 ' tsconfig.json > tsconfig.json.tmp
 mv tsconfig.json.tmp tsconfig.json
